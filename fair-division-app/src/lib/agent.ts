368a0c05ebb9045472aa478e22e17cf25499be26
Baran Açıkel <wbaranacikelw@gmail.com>
2025-03-26 15:44:59 +0100

    Update interface name

--- fair-division-app/src/lib/agent.ts
+++ fair-division-app/src/lib/agent.ts
@@ -1,6 +1,6 @@
 export type Color = 'red' | 'green' | 'blue' | 'yellow' | 'purple';
 
-export interface Agent {
+export interface IAgent {
 	name: string;
 	attributions: Record<Color, number>;
 	utilities: Record<Color, number>;
