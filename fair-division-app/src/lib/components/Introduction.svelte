acb8ff98c0d13a22616f0ea72eaca595994a2d1e
Baran Açıkel <wbaranacikelw@gmail.com>
2025-03-26 13:06:03 +0100

    Create introduction component

---
+++ fair-division-app/src/lib/components/Introduction.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import division from '../../static/division.webp';
+</script>
+
+<div class="bg-cover bg-center bg-no-repeat" style="background-image: url({division})">
+	<div
+		class="text-primary bg-slate-50 bg-opacity-35 px-20 py-36 text-center text-lg leading-relaxed tracking-wide"
+	>
+		Fair allocation is a key issue in economics and game theory, dealing with distributing
+		indivisible resources fairly among agents. This applies to inheritance, housing, and task
+		distribution. Since some goods (e.g., a house) cannot be split without losing value,
+		allocation methods aim to ensure fairness, efficiency, and feasibility. Research relies on
+		optimization, algorithms, and decision theory to maximize overall fairness and social
+		welfare.
+	</div>
+</div>
--- fair-division-app/src/routes/+page.svelte
+++ fair-division-app/src/routes/+page.svelte
@@ -1,5 +1,8 @@
 <script>
 	import AppHeader from '$lib/components/AppHeader.svelte';
+	import Introduction from '$lib/components/Introduction.svelte';
 </script>
 
 <AppHeader />
+
+<Introduction />
